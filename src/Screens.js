d874e7d39b0834ca6506efccaad112811ff8e2c0
Bartek-Nalepa <nalepabartek95@gmail.com>
2019-04-08 10:52:25 +0200

    All done

--- src/Screens.js
+++ src/Screens.js
@@ -1,5 +1,4 @@
 import {Navigation} from "react-native-navigation";
-import Settings from './Settings';
 import Calendar from './Calendar';
 import DaySelected from "./DaySelected";
 
@@ -7,5 +6,4 @@ export function registerScreens() {
 
     Navigation.registerComponent('Calendar', () => Calendar);
     Navigation.registerComponent('DaySelected', () => DaySelected);
-    Navigation.registerComponent('Settings', () => Settings);
 }
